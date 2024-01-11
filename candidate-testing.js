aa8e137bee0c4fabf56b14fd85c18cfde28ac5c0
Kobeburden1 <Kobeburden1@gmail.com>
2024-01-11 17:56:45 -0600

    CandidateTesting Part1 completed

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName=(" ");
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = ("Who was the first American woman in space? ");
+let correctAnswer = ("Sally Ride");
+let candidateAnswer = (" ");
 
 
 //TODO: Variables for Part 2
@@ -17,11 +17,14 @@ let candidateAnswers;
 
 
 function askForName() {
+let candidateName = input.question(("Enter your name: "))
   // TODO 1.1b: Ask for candidate's name //
 
 }
 
 function askQuestion() {
+  console.log(question);
+  let candidateAnswer = input.question("Awnser: ")
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
 
@@ -42,7 +45,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log("Hello," + candidateName + "!");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
