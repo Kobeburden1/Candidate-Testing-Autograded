ddf7c2404b8ed90e14cf81d30d292d6351925e13
Kobeburden1 <Kobeburden1@gmail.com>
2024-01-22 20:54:08 -0600

    CanTestP2 done

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,15 +5,19 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = ("Who was the first American woman in space? ");
+let question = ("Who was the first American woman in space? ")
 let correctAnswer = ("Sally Ride");
-let candidateAnswer = (" ");
+let candidateAnswer = " ";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [ "Who was the first American woman in space? ",
+"True or false: 5 kilometer == 5000 meters? ",
+"(5 + 3)/2 * 10 = ? ",
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+"What is the minimum crew size for the ISS? " ];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3" ];
+let candidateAnswers= [];
 
 
 function askForName() {
@@ -24,22 +28,22 @@ function askForName() {
 }
 
 function askQuestion() {
-  console.log(question);
-  let candidateAnswer = input.question(` Awnser: `)
+  
 
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+for (let i = 0; i<questions.length; i++) {
+  console.log(questions[i]);
+  candidateAnswer = input.question("Awnser: ")
+  candidateAnswers.push(candidateAnswer)
+ }
 }
-
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer === correctAnswer) {
-    console.log("Correct! ")
-  } else {
-    console.log("Incorrect. ")
-  }
+ console.log(`1. ${questions[0]} ${candidateName} awnsered: ${candidateAnswers[0]}. Correct awnser: ${correctAnswers[0]}\n
+ 2. ${questions[1]} ${candidateName} awnsered: ${candidateAnswers[1]}. Correct awnser: ${correctAnswers[1]}\n
+ 3. ${questions[2]} ${candidateName} awnsered: ${candidateAnswers[2]}. Correct awnser: ${correctAnswers[2]}\n
+ 4. ${questions[3]} ${candidateName} awnsered: ${candidateAnswers[3]}. Correct awnser: ${correctAnswers[3]}\n
+ 5. ${questions[4]} ${candidateName} awnsered: ${candidateAnswers[4]}. Correct awnser: ${correctAnswers[4]}  `)
 
 
 
